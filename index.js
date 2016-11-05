30b53e053db5e52495affa1583fcc1f9f571c7de
lzane <cn.zanelee@gmail.com>
2016-11-05 20:33:52 +0800

    RESTful route & CRUD camp & some css

--- index.js
+++ index.js
@@ -5,57 +5,97 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var methodOverride = require("method-override");
+mongoose.connect("mongodb://localhost/zanecamp");
+
+var campSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String,
+    created: {type: Date, default: Date.now()}
+});
+
+var Camp = mongoose.model("Camp", campSchema);
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
-
-var data = [{
-    name: "Kalen Emsley",
-    image: "https://images.unsplash.com/44/C3EWdWzT8imxs0fKeKoC_blackforrest.JPG?dpr=1.2000000476837158&auto=compress,format&fit=crop&w=1199&h=899&q=80&cs=tinysrgb&crop="
-    },
-    {
-        name: "Felix Russell-Saw",
-        image: "https://images.unsplash.com/reserve/Hxev8VTsTuOJ27thHQdK_DSC_0068.JPG?dpr=1.2000000476837158&auto=compress,format&fit=crop&w=1199&h=803&q=80&cs=tinysrgb&crop="
-    },
-    {
-        name: "E.T.",
-        image: "https://images.unsplash.com/photo-1472152083436-a6eede6efad9?dpr=1.100000023841858&auto=compress,format&fit=crop&w=1199&h=801&q=80&cs=tinysrgb&crop="
-    },
-    {
-        name: "Felix Russell-Saw",
-        image: "https://images.unsplash.com/reserve/Hxev8VTsTuOJ27thHQdK_DSC_0068.JPG?dpr=1.2000000476837158&auto=compress,format&fit=crop&w=1199&h=803&q=80&cs=tinysrgb&crop="
-    },
-    {
-        name: "E.T.",
-        image: "https://images.unsplash.com/photo-1472152083436-a6eede6efad9?dpr=1.100000023841858&auto=compress,format&fit=crop&w=1199&h=801&q=80&cs=tinysrgb&crop="
-    },
-    {
-        name: "Felix Russell-Saw",
-        image: "https://images.unsplash.com/reserve/Hxev8VTsTuOJ27thHQdK_DSC_0068.JPG?dpr=1.2000000476837158&auto=compress,format&fit=crop&w=1199&h=803&q=80&cs=tinysrgb&crop="
-    },
-    {
-        name: "E.T.",
-        image: "https://images.unsplash.com/photo-1472152083436-a6eede6efad9?dpr=1.100000023841858&auto=compress,format&fit=crop&w=1199&h=801&q=80&cs=tinysrgb&crop="
-    }];
+app.use(methodOverride("_method"));
 
 
-app.get("/showCamp", function (req, res) {
-    res.render("showCamp", {data: data});
+app.get("/camps", function (req, res) {
+    Camp.find({}, function (err, camps) {
+        if (err) {
+            console.log("DB find Error!");
+        } else {
+            res.render("camps", {data: camps});
+        }
+    });
 });
 
-app.post("/showCamp",function (req, res) {
-    var receive_date = req.body;
-    data.push(receive_date);
-    res.redirect("/showCamp");
+app.post("/camps", function (req, res) {
+    Camp.create(req.body.camp, function (err) {
+        if (err) {
+            console.log("DB insert Error!");
+        } else {
+            res.redirect("/camps");
+        }
+    });
 });
 
-app.get("/createcamp", function (req, res) {
-   res.render("createCamp");
+
+app.get("/camps/new", function (req, res) {
+    res.render("createCamp");
 });
 
-app.get("*",function (req, res) {
-   res.send("No route handled this request!");
+app.get("/camps/:id", function (req, res) {
+    var id = req.params.id;
+    Camp.findById(id, function (err, foundCamp) {
+        if (err) {
+            res.send("can not find the camp");
+        } else {
+            res.render("campDetail", {camp: foundCamp});
+        }
+    });
+});
+
+app.get("/camps/:id/edit", function (req, res) {
+    var id = req.params.id;
+    Camp.findById(id, function (err, foundCamp) {
+        if (err) {
+            res.send("can not find the camp");
+        } else {
+            res.render("campEdit", {camp: foundCamp});
+        }
+    });
+});
+
+app.put("/camps/:id", function (req, res) {
+    var camp = req.body.camp;
+    var id = req.params.id;
+    Camp.findByIdAndUpdate(id, camp, function (err, updateCamp) {
+        if (err) {
+            res.send("can not update camp");
+        } else {
+            res.redirect("/camps/" + id);
+        }
+    })
+});
+
+app.delete("/camps/:id", function (req, res) {
+    var id = req.params.id;
+    Camp.findByIdAndRemove(id,function (err) {
+        if (err){
+            res.send("can not delete camp");
+        }else{
+            res.redirect("/camps");
+        }
+    })
+});
+
+app.get("*", function (req, res) {
+    res.send("No route handled this request!");
 });
 
 app.listen(3000, function (req, res) {
